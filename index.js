9f897df2ab455d57a181644f67b1cfc27f7f21b0
naeem <naeemislamjr182@gmail.com>
2023-10-14 02:47:12 +0600

    app.post----->insert  and   app.get------>read

--- index.js
+++ index.js
@@ -35,11 +35,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
 
-
-
-
+        //from add coffe
+        app.post('/coffee', async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee);
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+        //read 
+        app.get('/coffee', async (req, res) => {
+            const cursor = coffeeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
 
 

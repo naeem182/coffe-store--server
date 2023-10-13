918157e05ca74a47a21307502e75552694b02566
naeem <naeemislamjr182@gmail.com>
2023-10-14 04:36:00 +0600

    app.delete    specific id

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //midleware
 app.use(cors());
 app.use(express.json());
@@ -52,6 +52,13 @@ async function run() {
             res.send(result);
         })
 
+        //delete
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection

7b4ca55f88b5a3b5e266aa3d7b8e9ce4e2198af9
naeem <naeemislamjr182@gmail.com>
2023-10-14 04:51:29 +0600

    app.get   specifi id get for update

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
             const result = await coffeeCollection.deleteOne(query);
             res.send(result);
         })
+        //specifi id get for update
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
